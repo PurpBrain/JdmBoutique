e867f41e69e5011d15acaab992bc35695f702733
PurpBrain <leo.doray@gmail.com>
2021-11-30 14:13:15 +0100

    search filter

--- app.js
+++ app.js
@@ -1,19 +1,28 @@
+//Définir l'interface
+const searchBarFilter = document.querySelector('#search');
+
+//Application
+searchBar()
+function searchBar() {
+    //Barre de recherche 
+    searchBarFilter.addEventListener('keyup', resultatRecherche);
+}
+
+//Local Storage
 function saveUser() {
     let addUser = document.getElementById('dname').value;
-    //console.log(addUser);
     localStorage.setItem('userName', addUser)
 }
+
+//Dark mode 
 function darkMode() {
+
     // Background
     let modeDark = document.body;
-    
-    //console.log('mode dark', modeDark)
-    
-    
+            
     // Text
     let allBlackTheme = document.querySelectorAll("h1, h2, h3, label, input"); 
     let textWhite;
-    //console.log('test', allBlackTheme[0].style)
     var element = document.getElementById("ziak");
 
     if(element.style.color == 'white'){
@@ -30,6 +39,18 @@ function darkMode() {
             
         }
     } 
-     
-    //console.log(allBlackTheme);
+}
+// Barre de recherche fonction
+function resultatRecherche(e) {
+    const contenuClavier = e.target.value.toLowerCase();
+    document.querySelectorAll('.product-item').forEach(
+        function(tache) {
+            const motCle = tache.firstChild.textContent
+            if (motCle.toLocaleLowerCase().indexOf(contenuClavier)!= -1) {
+                tache.getElementsByClassName.display = 'block'
+            } else {
+                tache.style.display = 'none'
+            } 
+        }
+    )
 }
