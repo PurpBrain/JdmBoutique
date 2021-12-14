396d31586fc313d7419924c36a739eb39c38d578
Axel <axel.etoile.espoire@gmail.com>
2021-12-14 20:28:23 +0100

    Fix BlogID

--- back/controllers/IdBlogController.js
+++ back/controllers/IdBlogController.js
@@ -7,17 +7,18 @@ const voiture = require("../../public/data/db.json").fiche;
 exports.idblogpage = (req, res) => {
     console.log('je suis la page idblog');
 
-    // let voitureItem = {}
+    let voitureItem = {}
 
-    // voiture.forEach(art => {
-    //     if (art.id === Number(req.params.id)) {
-    //         voitureItem = art
-    //         console.log(voitureItem);
-    //     }        
-    // })
-
+    voiture.forEach(art => {
+        if (art.id === Number(req.params.id)) {
+            voitureItem = art
+        }        
+    })
+    
+    var nbr = Number(voitureItem.id)
+    nbr-=1
     res.render('idblog', {
-        voiture: voiture[req.params.id],
-        pathimg: `${voiture[req.params.id].img_url}`
+        voiture: voiture[nbr],
+        pathimg: `${voiture[nbr].img_url}`
     });
 }
