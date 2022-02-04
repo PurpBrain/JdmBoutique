c742afb5fa3333c7432d2c666440b057e9862fed
PurpBrain <leo.doray@gmail.com>
2022-02-04 15:45:00 +0100

    add

--- back/controllers/AdminController.js
+++ back/controllers/AdminController.js
@@ -6,11 +6,7 @@
 
 exports.adminpage = (req, res) => {
     console.log('Page admin');
-    // res.render("admin", {
-    //     article: await db.query("SELECT * FROM article;"),
-    //     image: await db.query("SELECT DISTINCT img_url FROM image;")
-    //   });
-    // let sql = `SELECT * FROM article`;
+    
     let sqlGetImg = `SELECT image.id_img, image.img_url, article.id_Article, article.make, article.model
                         FROM article 
                         INNER JOIN image
@@ -21,7 +17,6 @@ exports.adminpage = (req, res) => {
         res.render('admin', {
             voiture: data
         });
-        // res.json({data})
     })
 
 
@@ -34,7 +29,6 @@ exports.getVoiture = (req, res) => {
     var numPages;
     var skip = page * numPerPage;
     var limit = skip + ',' + numPerPage;
-    console.log("voiture", req.query.page)
 
     let sql = `SELECT count(*) as numRows FROM article`;
     db.query(sql, (error, results, fields) => {
@@ -69,41 +63,6 @@ exports.addVoiture = async (req, res) => {
         const update_cars = await db.query(`UPDATE article SET img_id ='${picture_insert.insertId}' WHERE id_Article='${cars_insert.insertId}'`);
     }
 
-    //console.log(cars_insert.insertId);
-
-    // let sql = `INSERT INTO article SET make=?, model=?, price=?, author_id=?, description=?`;
-
-    // let values = [
-    //     req.body.make,
-    //     req.body.model,
-    //     req.body.price,
-    //     1,
-    //     req.body.description
-    // ];
-
-    //let sqlGet = `SELECT id_Article FROM article ORDER BY id_Article  DESC LIMIT 1`;
-    // db.query(sqlGet, function (err, data_id, fields) {
-    //     if (err) throw err;
-    //     for (i = 0; i < req.files.length; i++) {
-    //         let sqlSet = `INSERT INTO image SET img_url=?, id_article=?`;
-
-    //         let values = [
-    //             req.files[i].filename,
-    //             data_id[0].id_Article
-    //         ]
-
-    //         db.query(sqlSet, values, function (err, data3, fields) {
-    //             if (err) throw err;
-
-    //             console.log("test");
-
-    //             let sqlUpdate = `UPDATE article SET img_id = ? WHERE id_Article= ?`
-
-    //         })
-    //     }
-    // })
-
-
     // SQL récupération de tout les articles
     let sql = `SELECT * FROM article`;
     db.query(sql, (error, dataRes, fields) => {
@@ -114,11 +73,6 @@ exports.addVoiture = async (req, res) => {
 }
 
 exports.delVoiture = (req, res) => {
-    // let sql = `DELETE image,article  
-    //            FROM image
-    //            INNER JOIN article  
-    //            ON image.id_article = article.img_id
-    //            WHERE article.id_Article= ?`
     let sql = `DELETE image,article 
                FROM image
                INNER JOIN article
--- back/controllers/HomeController.js
+++ back/controllers/HomeController.js
@@ -5,8 +5,8 @@
 // Controller pour la page home
 exports.homepage = (req, res) => {
     console.log('Page home');
-    console.log('session',req.session)
-    let sql = `SELECT * FROM article INNER JOIN image ON image.id_article = article.img_id;`;
+    // console.log('session',req.session)
+    let sql = `SELECT * FROM article INNER JOIN image ON image.id_article = article.img_id ORDER BY article.id_Article DESC;`;
     db.query(sql, (error, data, fields) => {
         if (error) throw error;
         
